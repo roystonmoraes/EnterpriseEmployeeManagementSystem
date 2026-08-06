0be0c8341907460548040aac5c9a0c61b9376772
Royston Moraes <royston769@live.com>
2026-08-06 16:36:44 +0530

    feat(api): configure web api project

---
+++ src/EnterpriseEmployeeManagementSystem.API/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseEmployeeManagementSystem.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            Status = "Healthy",
+            Version = "1.0.0",
+            Timestamp = DateTime.UtcNow
+        });
+    }
+}
---
+++ src/EnterpriseEmployeeManagementSystem.API/Program.cs
@@ -0,0 +1,23 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+// Swagger / OpenAPI
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
