c3b1be6b0ce0a14c6bc0b165d02e6fb909485c32
Royston Moraes <royston769@live.com>
2026-08-11 17:50:51 +0530

    feat(api): add employee controller

---
+++ src/EnterpriseEmployeeManagementSystem.API/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using EnterpriseEmployeeManagementSystem.Application.DTOs.Employees;
+using EnterpriseEmployeeManagementSystem.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnterpriseEmployeeManagementSystem.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeeController : ControllerBase
+{
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeeController(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<EmployeeDto>> Create(
+        CreateEmployeeRequest request,
+        CancellationToken cancellationToken)
+    {
+        var employee = await _employeeService.CreateAsync(
+            request,
+            cancellationToken);
+
+        return CreatedAtAction(
+            nameof(Create),
+            new { id = employee.Id },
+            employee);
+    }
+}
--- src/EnterpriseEmployeeManagementSystem.API/Program.cs
+++ src/EnterpriseEmployeeManagementSystem.API/Program.cs
@@ -7,21 +7,17 @@ builder.Services.AddApplication();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// Add services
 builder.Services.AddControllers();
-
-// Swagger / OpenAPI
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure pipeline
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-//app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
